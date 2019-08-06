504d5238c5085dc1616157f569afcc2307ce9a4e
yaoyonstudio <yaoyonstudio@gmail.com>
2019-08-06 14:48:40 +0800

    ''

--- pgsite/mainsite/urls.py
+++ pgsite/mainsite/urls.py
@@ -1,13 +1,12 @@
-from django.contrib import admin
+""" module """
+
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from graphene_django.views import GraphQLView
-
 
 from . import  views
 
 urlpatterns = [
-    path('main/', views.main, name = 'mainsite.main'),
+    path('main/', views.main, name='mainsite.main'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
